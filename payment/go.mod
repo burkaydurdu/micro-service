298d3b564472916fa4c6b5588a600c13c057fb18
Burkay Durdu <burkaydurdu@outlook.com>
2022-01-22 19:02:40 +0300

    Add payment service

---
+++ payment/go.mod
@@ -0,0 +1,3 @@
+module github.com/burkaydurdu/micro-service/payment
+
+go 1.17
---
+++ payment/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/turbine/config", turbineConfigHandler)
+	http.HandleFunc("/payments", createPaymentHandler)
+	log.Println("payment-service is available at localhost:3002")
+	log.Fatal(http.ListenAndServe(":3002", nil))
+}
+
+func createPaymentHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(fmt.Sprintf(`{"message": "paid"}`)))
+}
+
+func turbineConfigHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(fmt.Sprintf(`{"serviceName": "payment-service"}`)))
+}
